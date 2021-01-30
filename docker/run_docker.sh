9f97e931fafafbe32cbc438804146a51219501b8
fkubota <rkkubotay@gmail.com>
2021-01-30 14:33:48 +0900

    [update] run_docker.sh, g=-1の場合分けを追加

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+# 引数チェック
 if [ $# = 0 ]; then
     echo 引数を入れてください
     exit 1
 fi
 
+# 引数取得p, g
 while getopts "p:g:" OPT
   do  
     case $OPT in
@@ -13,5 +15,10 @@ while getopts "p:g:" OPT
       \? ) usage ;;
     esac
   done 
-sudo docker run --rm -it --shm-size=8g --gpus $GPU -p $PORT:$PORT -v /etc/localtime:/etc/localtime:ro -v /home/fkubota/Git/:/home/user/work/ fkubota/rfcx bash
 
+# g=-1 なら--gpusオプションを削除
+if [ $GPU = -1 ]; then
+	sudo docker run --rm -it --shm-size=8g -p $PORT:$PORT -v /etc/localtime:/etc/localtime:ro -v /home/fkubota/Git/:/home/user/work/ fkubota/rfcx bash
+else
+	sudo docker run --rm -it --shm-size=8g --gpus $GPU -p $PORT:$PORT -v /etc/localtime:/etc/localtime:ro -v /home/fkubota/Git/:/home/user/work/ fkubota/rfcx bash
+fi
