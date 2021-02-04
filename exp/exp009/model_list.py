e30ea586803577d8b78c7e94cc3eefde492746e0
fkubota <rkkubotay@gmail.com>
2021-02-04 10:42:32 +0900

    [update] model_list, resnet18_2を追加。gap+gmpを実装

--- exp/exp009/model_list.py
+++ exp/exp009/model_list.py
@@ -52,3 +52,30 @@ class ResNet50(nn.Module):
             "output_softmax": multiclass_proba,
             "output_sigmoid": multilabel_proba
         }
+
+
+class ResNet18_2(nn.Module):
+    '''
+    self.resnet.avgpoolにGAPとGMPの和を追加
+    '''
+    def __init__(self, params):
+        super().__init__()
+        self.__class__.__name__ = 'ResNet18'
+        num_classes = params['n_classes']
+        pretrained = params['pretrained']
+
+        self.resnet = models.resnet18(pretrained=pretrained)
+        in_features = self.resnet.fc.in_features
+        self.resnet.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))\
+            + nn.AdaptiveMaxPool2d(output_size=(1, 1))
+        self.resnet.fc = nn.Linear(in_features, num_classes)
+
+    def forward(self, x):
+        x = self.resnet(x)
+        multiclass_proba = F.softmax(x, dim=1)
+        multilabel_proba = torch.sigmoid(x)
+        return {
+            "output": x,
+            "output_softmax": multiclass_proba,
+            "output_sigmoid": multilabel_proba
+        }
