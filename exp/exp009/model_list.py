f825d151bcc2573ca4344f34445442e3ac5d4358
fkubota <rkkubotay@gmail.com>
2021-02-04 21:32:39 +0900

    [update] run exp009

--- exp/exp009/model_list.py
+++ exp/exp009/model_list.py
@@ -17,7 +17,10 @@ class ResNet18(nn.Module):
 
         self.resnet = models.resnet18(pretrained=pretrained)
         in_features = self.resnet.fc.in_features
-        self.resnet.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        # self.resnet.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        self.resnet.avgpool = nn.AdaptiveMaxPool2d(output_size=(1, 1))
+        # self.resnet.avgpool = nn.AdaptiveMaxPool2d(output_size=(1, 1)) +\
+            # nn.AdaptiveAvgPool2d(output_size=(1, 1))
         self.resnet.fc = nn.Linear(in_features, num_classes)
 
     def forward(self, x):
@@ -41,33 +44,6 @@ class ResNet50(nn.Module):
         self.resnet = models.resnet50(pretrained=pretrained)
         in_features = self.resnet.fc.in_features
         self.resnet.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
-        self.resnet.fc = nn.Linear(in_features, num_classes)
-
-    def forward(self, x):
-        x = self.resnet(x)
-        multiclass_proba = F.softmax(x, dim=1)
-        multilabel_proba = torch.sigmoid(x)
-        return {
-            "output": x,
-            "output_softmax": multiclass_proba,
-            "output_sigmoid": multilabel_proba
-        }
-
-
-class ResNet18_2(nn.Module):
-    '''
-    self.resnet.avgpoolにGAPとGMPの和を追加
-    '''
-    def __init__(self, params):
-        super().__init__()
-        self.__class__.__name__ = 'ResNet18'
-        num_classes = params['n_classes']
-        pretrained = params['pretrained']
-
-        self.resnet = models.resnet18(pretrained=pretrained)
-        in_features = self.resnet.fc.in_features
-        self.resnet.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))\
-            + nn.AdaptiveMaxPool2d(output_size=(1, 1))
         self.resnet.fc = nn.Linear(in_features, num_classes)
 
     def forward(self, x):
