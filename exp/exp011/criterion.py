5a10f7240c518e9c71dd61b2bda60bd9db1b1eb9
fkubota <rkkubotay@gmail.com>
2021-02-05 09:27:27 +0900

    [add] exp011(baseexp010)

---
+++ exp/exp011/criterion.py
@@ -0,0 +1,31 @@
+from torch import nn
+
+
+class ResNetLoss(nn.Module):
+    def __init__(self, loss_type="ce"):
+        super().__init__()
+
+        self.loss_type = loss_type
+        if loss_type == "ce":
+            self.loss = nn.CrossEntropyLoss()
+        elif loss_type == "bce":
+            self.loss = nn.BCELoss()
+        elif loss_type == "bcew":
+            self.loss = nn.BCEWithLogitsLoss()
+
+    def forward(self, input_, target):
+        if self.loss_type == "ce":
+            input_ = input_["output_sigmoid"]
+            target = target.argmax(1).long()
+        elif self.loss_type == "bce":
+            input_ = input_["output_sigmoid"]
+            target = target.float()
+        elif self.loss_type == "bcew":
+            input_ = input_["output"]
+            target = target.float()
+
+        return self.loss(input_, target)
+
+
+def mixup_criterion(criterion, pred, y_a, y_b, lam):
+    return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
