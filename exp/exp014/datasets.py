893ed498b361da74c2de857459f639477aaf568c
fkubota <rkkubotay@gmail.com>
2021-02-06 15:31:58 +0900

    [add] exp014

---
+++ exp/exp014/datasets.py
@@ -0,0 +1,114 @@
+from icecream import ic
+from ipdb import set_trace as st
+from loguru import logger
+import cv2
+import librosa
+import librosa.display
+import numpy as np
+import soundfile as sf
+import torch.utils.data as data
+
+
+def mono_to_color(X: np.ndarray,
+                  mean=None,
+                  std=None,
+                  norm_max=None,
+                  norm_min=None,
+                  eps=1e-6):
+    # Stack X as [X,X,X]
+    X = np.stack([X, X, X], axis=-1)
+
+    # Standardize
+    mean = mean or X.mean()
+    X = X - mean
+    std = std or X.std()
+    Xstd = X / (std + eps)
+    _min, _max = Xstd.min(), Xstd.max()
+    norm_max = norm_max or _max
+    norm_min = norm_min or _min
+    if (_max - _min) > eps:
+        # Normalize to [0, 255]
+        V = Xstd
+        V[V < norm_min] = norm_min
+        V[V > norm_max] = norm_max
+        V = 255 * (V - norm_min) / (norm_max - norm_min)
+        V = V.astype(np.uint8)
+    else:
+        # Just zero
+        V = np.zeros_like(Xstd, dtype=np.uint8)
+    return V
+
+
+class SpectrogramDataset(data.Dataset):
+    def __init__(self,
+                 df_trn_tp,
+                 dir_data,
+                 phase,
+                 config):
+        self.df_trn_tp = df_trn_tp
+        # self.unique_rec = df_trn_tp['recording_id'].unique()
+        self.dir_data = dir_data
+        self.img_size = config['params']['img_size']
+        self.period = config['params']['period']
+        self.shift_duration = config['params']['shift_duration']
+        self.melspec_params = config['melspec_params']
+
+    def __len__(self):
+        return len(self.df_trn_tp)
+
+    def __getitem__(self, idx):
+        series = self.df_trn_tp.iloc[idx, :]
+        rec = series['recording_id']
+        path_flac = f'{self.dir_data}{rec}.flac'
+        species_id = series["species_id"]
+        t_min = series['t_min']
+
+        # load
+        y, sr = sf.read(path_flac)
+
+        # ランダムに切り取る範囲の左端
+        effective_length = sr * self.period
+        # t_left = t_min - self.shift_duration
+
+        # ラベル無視で完全にランダムにクロップ
+        start = int(np.random.randint(len(y) - effective_length))
+
+        # spectrogramの計算
+        y_crop = y[start:start+effective_length].astype(np.float32)
+
+        # logger.info(f'y_crop: {len(y_crop)}')
+        melspec = librosa.feature.melspectrogram(
+                y_crop,
+                sr=sr,
+                **self.melspec_params)
+        melspec = librosa.power_to_db(melspec).astype(np.float32)
+
+        # ----- 可視化 -----
+        # t_max = series['t_max']
+        # librosa.display.specshow(
+        #         # melspec, sr=sr, x_axis='time', y_axis='mel')
+        #         melspec, sr=sr, x_axis='time', y_axis='mel')
+        # import matplotlib.pyplot as plt
+        # plt.title(f'{rec} [{t_min}~{t_max}],'
+        #           f'[{start/sr:.1f}~{(start+effective_length)/sr:.1f}]')
+        # plt.show()
+        # -----
+
+        # 入力画像の加工
+        image = mono_to_color(melspec)
+        height, width, _ = image.shape
+        image = cv2.resize(
+                image,
+                (int(width * self.img_size / height), self.img_size)
+                )
+        image = np.moveaxis(image, 2, 0)
+        image = (image / 255.0).astype(np.float32)
+
+        # ラベルの作成
+        labels = np.zeros(self.df_trn_tp.species_id.nunique(), dtype=int)
+        labels[species_id] = 1
+        # logger.info(f't_left: {t_left}')
+        # logger.info(f'len(y_crop): {len(y_crop)}')
+        # logger.info(f'start_sec: {start/sr:.3f}')
+        # logger.info(f'image.shape: {image.shape}\n')
+        return image, labels
---
+++ exp/exp014/exp.py
@@ -0,0 +1,65 @@
+from icecream import ic
+from ipdb import set_trace as st
+import os
+import time
+import yaml
+from loguru import logger
+
+import utils as U
+import trainner
+import configuration as C
+import result_handler as rh
+
+
+def run_exp(run_name, config_update):
+    # start
+    start = time.time()
+    pwd = os.path.dirname(os.path.abspath(__file__))
+    with open(f'{pwd}/config.yml', 'r') as yml:
+        config = yaml.safe_load(yml)
+
+    # init
+    dir_save, _, config = U.init_exp(config, config_update, run_name)
+    config = U.set_debug_config(config)
+    _, _, exp_name = U.get_save_dir_exp(config, run_name)
+    rh.save_model_architecture(dir_save, C.get_model(config))
+
+    # train
+    logger.info('='*30)
+    logger.info(f'::: {exp_name}_{run_name} start :::')
+    logger.info('='*30)
+    trainner.train_cv(config, run_name)
+
+    # end
+    end = time.time()
+    time_all = end - start
+    logger.info(f'elapsed time: {U.sec2time(time_all)}')
+    logger.info(f'::: {exp_name}_{run_name} end :::\n\n')
+
+
+def start_sweep_dict(list_config_str):
+    '''
+    dictのリスト分の掃引
+    '''
+    logger.remove()
+    logger.add('exp.log', mode='w')
+
+    for i_run, config_str in enumerate(list_config_str, 1):
+        config_update = yaml.safe_load(config_str)
+        run_name = f'run{str(i_run).zfill(3)}'
+        run_exp(run_name, config_update)
+
+
+def main():
+    list_config_str = [
+            '''
+            globals:
+                debug: False
+            ''',
+            ]
+
+    start_sweep_dict(list_config_str)
+
+
+if __name__ == "__main__":
+    main()
