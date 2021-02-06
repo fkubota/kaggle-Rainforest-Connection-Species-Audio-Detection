6640d568a8fbdd7e48a15f56e3a1483906dd7299
fkubota <rkkubotay@gmail.com>
2021-02-06 15:46:22 +0900

    [update] exp014 utils.pyにplot_cunfusion_matrixを追加

--- exp/exp014/utils.py
+++ exp/exp014/utils.py
@@ -1,11 +1,14 @@
 from ipdb import set_trace as st
 import os
 import yaml
-import subprocess
+import torch
 import random
+import itertools
+import subprocess
 import numpy as np
 from loguru import logger
-import torch
+import matplotlib.pyplot as plt
+from sklearn.metrix import confusion_matrix
 
 
 def init_exp(config, config_update, run_name):
@@ -166,3 +169,29 @@ def LWLRAP(preds, labels):
     scores = score_matrix * score_mask_matrix
     score = scores.sum() / labels.sum()
     return score.item()
+
+
+def plot_confusion_matrix(truth, pred, classes, normalize=False, title=''):
+    cm = confusion_matrix(truth, pred)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.figure(figsize=(10, 10))
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title('Confusion matrix', size=15)
+    plt.colorbar(fraction=0.046, pad=0.04)
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.grid(False)
+    plt.tight_layout()
